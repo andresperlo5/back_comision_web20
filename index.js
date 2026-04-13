0395cfa0e4dcbd03d6dbfccd31a80c2f89591fac
unknown <andresperlo5@gmail.com>
2026-04-13 20:38:55 -0300

    agregamos express y el flag watch

---
+++ index.js
@@ -0,0 +1,7 @@
+// import express from "express"; //module //babel // webpack
+const express = require("express"); //conmonJS
+const app = express();
+
+app.listen(3001, () => {
+  console.log("el servidor esta encendido:", 3001);
+});
